1b605e358dcd165932fff0c78d7720aa979cad55
Nico Prananta <nicnocquee@yahoo.com>
2015-05-21 15:06:49 +0900

    placeholder and more baits

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -16,7 +16,7 @@ angular.module('clickbaitApp')
     ];
 
     $scope.buttonDisplay = false;
-    //$scope.bait = 'You wont believe what happens next.';
+
     $scope.baits = ['At first you\'ll be intrigued. Then you\'ll be blown away.',
                     'I\'m still trying to pick my jaw up off the floor.',
                     'If You Can Watch This And Not Feel Disgusted, Then You Have No Emotions.',
@@ -27,7 +27,24 @@ angular.module('clickbaitApp')
                     'Just try not to stare at it.',
                     'There is no God.',
                     'You\'re gonna weep for the future.',
-                    'Here\'s why.'
+                    'Here\'s why.',
+                    'My God, you HAVE to see this.',
+                    'It just blows my mind.',
+                    'You will never think the same way again.',
+                    'I dare you to watch this.',
+                    'I can\'t believe what I just read.',
+                    'I can\'t believe what I just watched.',
+                    'I can\'t believe what I just saw.',
+                    'You\'ve been doing it wrong all this time.',
+                    'You think you know it well. But you know nothing, Jon Snow!',
+                    'You will never see it the same way again.',
+                    'Forget what you know because you know nothing actually.',
+                    'Belive it or not, it changed his life.',
+                    'Belive it or not, it changed her life.',
+                    'We can close the internet now.',
+                    'So hilarious you will die.',
+                    'What happens next is beyond this world.',
+                    'What happens next is more than what you can imagine.',
     ];
 
     $scope.result = function() {
