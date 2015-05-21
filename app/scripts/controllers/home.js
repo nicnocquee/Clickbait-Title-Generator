ca88af20f851d37c2cb86a5869402c082740946d
Nico Prananta <nicnocquee@yahoo.com>
2015-05-21 14:13:59 +0900

    generate random

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -15,7 +15,33 @@ angular.module('clickbaitApp')
       'Karma'
     ];
 
+    $scope.buttonDisplay = false;
+    //$scope.bait = 'You wont believe what happens next.';
+    $scope.baits = ['At first you\'ll be intrigued. Then you\'ll be blown away.',
+                    'I\'m still trying to pick my jaw up off the floor.',
+                    'If You Can Watch This And Not Feel Disgusted, Then You Have No Emotions.',
+                    'Try Not To Groan When You See It.',
+                    'You won\'t believe what happens next.',
+                    'First you\'ll be shocked then you\'ll be inspired.',
+                    'You won\'t believe it actually happened.',
+                    'Just try not to stare at it.',
+                    'There is no God.',
+                    'You\'re gonna weep for the future.',
+                    'Here\'s why.'
+    ];
+
     $scope.result = function() {
-      return $scope.who+' '+$scope.doing+'. You wont believe what happens next.';
-    }
+      if (!$scope.who || !$scope.doing) {
+        $scope.buttonDisplay = false;
+        return '';
+      }
+      $scope.buttonDisplay = true;
+      return $scope.who+' '+$scope.doing+'. '+$scope.bait;
+    };
+
+    $scope.generate = function () {
+      $scope.bait = $scope.baits[Math.floor(Math.random()*$scope.baits.length)];
+    };
+
+    $scope.generate();
   });
